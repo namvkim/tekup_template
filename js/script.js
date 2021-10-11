4e6c9b04f65053c8c322aa9db128dd87611c83fc
anhnguyen21 <anhanh5811@gmail.com>
2021-10-12 01:39:45 +0700

    Update footer and perfect service

--- js/script.js
+++ js/script.js
@@ -16,3 +16,32 @@ items.forEach((el) => {
         next = next.nextElementSibling;
     }
 });
+
+//start Hoa write carosel for perfect service
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("perfectservices__mySlides");
+  var dots = document.getElementsByClassName("perfect__dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" activeperfect", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " activeperfect";
+}
+//end Hoa write
