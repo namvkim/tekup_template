055d4db4a1922c418819e5bab3590e5f8a0cf49e
quynhhtn.wiicamp <quynhhtn@wiicamp.com>
2021-10-12 01:24:23 +0700

    services and section counter

--- js/script.js
+++ js/script.js
@@ -1,18 +1,55 @@
-// carosel for logo in banner 
+// carosel for logo in banner
 let items = document.querySelectorAll(
-    ".hompage_banner_logo .carousel .carousel-item"
+  '.hompage_banner_logo .carousel .carousel-item'
 );
 
 items.forEach((el) => {
-    const minPerSlide = 5;
-    let next = el.nextElementSibling;
-    for (var i = 1; i < minPerSlide; i++) {
-        if (!next) {
-            // wrap carousel by using first child
-            next = items[0];
-        }
-        let cloneChild = next.cloneNode(true);
-        el.appendChild(cloneChild.children[0]);
-        next = next.nextElementSibling;
+  const minPerSlide = 5;
+  let next = el.nextElementSibling;
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items[0];
     }
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
 });
+
+// services
+$(document).ready(function () {
+  $('.counter').each(function () {
+    $(this)
+      .prop('Counter', 0)
+      .animate(
+        {
+          Counter: $(this).text(),
+        },
+        {
+          duration: 4000,
+          easing: 'swing',
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          },
+        }
+      );
+  });
+});
+//
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName(
+    'homepage__services__tabcontent'
+  );
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = 'none';
+  }
+  tablinks = document.getElementsByClassName('homepage__services__tablinks');
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(' active', '');
+  }
+  document.getElementById(cityName).style.display = 'block';
+  evt.currentTarget.className += ' active';
+}
+document.getElementById('defaultOpen').click();
