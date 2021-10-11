85497fd65768730ba5bac10bcfa2c40800688fc7
namvkim <sonnamttt@gmail.com>
2021-10-11 19:46:21 +0700

    add menu

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// carosel for logo in banner 
+let items = document.querySelectorAll(
+    ".hompage_banner_logo .carousel .carousel-item"
+);
+
+items.forEach((el) => {
+    const minPerSlide = 5;
+    let next = el.nextElementSibling;
+    for (var i = 1; i < minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+            next = items[0];
+        }
+        let cloneChild = next.cloneNode(true);
+        el.appendChild(cloneChild.children[0]);
+        next = next.nextElementSibling;
+    }
+});
