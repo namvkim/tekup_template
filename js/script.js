44425846f820ec0114b890d4da5bef423f5bfafb
quynhhtn.wiicamp <quynhhtn@wiicamp.com>
2021-10-13 23:39:23 +0700

    update trang home services

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
       );
   });
 });
-//
+
 function openCity(evt, cityName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName(
@@ -45,11 +45,12 @@ function openCity(evt, cityName) {
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = 'none';
   }
-  tablinks = document.getElementsByClassName('homepage__services__tablinks');
+  tablinks = document.getElementsByClassName('tablinks');
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(' active', '');
   }
   document.getElementById(cityName).style.display = 'block';
   evt.currentTarget.className += ' active';
 }
+
 document.getElementById('defaultOpen').click();
