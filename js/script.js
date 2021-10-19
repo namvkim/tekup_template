cef03cd6b62927e6874dd0c730b9d692e1d4e238
Mai Hoa <hoa.le22@student.passerellesnumeriques.org>
2021-10-19 20:03:22 +0700

    Update outo slideshow, footer

--- js/script.js
+++ js/script.js
@@ -18,30 +18,23 @@ items.forEach((el) => {
 });
 
 //start Hoa write carosel for perfect service
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
+showSlides();
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides() {
   var i;
   var slides = document.getElementsByClassName("homepage__reviews__mySlides");
   var dots = document.getElementsByClassName("homepage__reviews__dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+    slides[i].style.display = "none";  
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" activeperfect", "");
+    dots[i].className = dots[i].className.replace("activeperfect", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " activeperfect";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 //end Hoa write
