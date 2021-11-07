c0e80cdb6d05499fc17495602dcc55acd0bfd2b0
namvkim <sonnamttt@gmail.com>
2021-11-08 00:03:50 +0700

    start reponse homepage service

--- js/script.js
+++ js/script.js
@@ -1,116 +1,3 @@
-if (document.getElementById('homepage')) {
-  // auto type letter in banner 
-  let typeEffect = (arr) => {
-    // let i = 0;
-    // let count = 0;
-    // let arrSize = arr.length;
-    // setInterval(function () {
-    //   // for (i = 0; i < arr[count].length; i++) {
-
-    //   // }
-    //   if (i < arr[count].length) {
-    //     document.getElementById("homepage__banner__title").append(arr[count].charAt(i));
-    //     i++;
-    //   } else {
-    //     count++;
-    //     if (count === arrSize) count = 0;
-    //     i = 0;
-    //     // setTimeout(function () { alert("After 5 seconds!"); }, 5000);
-    //     document.getElementById("homepage__banner__title").innerHTML = "";
-    //   }
-    // }, 150);
-
-
-  };
-
-  const arr = [
-    "Dẫn đầu xu hướng, nắm bắt tương lai",
-    "Khởi dậy đam mê",
-    "Kết nối mọi người",
-    "Đà Nẵng – Thành phố không bao giờ ngủ",
-    "Hãy là tất cả những gì bạn muốn",
-  ];
-
-  typeEffect(arr);
-
-  // carosel for logo in banner (nam)
-  let items = document.querySelectorAll(
-    ".hompage__banner__logo .carousel .carousel-item"
-  );
-
-  items.forEach((el) => {
-    const minPerSlide = 5;
-    let next = el.nextElementSibling;
-    for (var i = 1; i < minPerSlide; i++) {
-      if (!next) {
-        // wrap carousel by using first child
-        next = items[0];
-      }
-      let cloneChild = next.cloneNode(true);
-      el.appendChild(cloneChild.children[0]);
-      next = next.nextElementSibling;
-    }
-  });
-
-  //carosel for perfect service
-  var slideIndex = 0;
-  showSlides();
-
-  function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("homepage__reviews__mySlides");
-    var dots = document.getElementsByClassName("homepage__reviews__dot");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace("activeperfect", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " activeperfect";
-    setTimeout(showSlides, 2000);
-  }
-
-  // services
-  $(document).ready(function () {
-    $('.counter').each(function () {
-      $(this)
-        .prop('Counter', 0)
-        .animate(
-          {
-            Counter: $(this).text(),
-          },
-          {
-            duration: 4000,
-            easing: 'swing',
-            step: function (now) {
-              $(this).text(Math.ceil(now));
-            },
-          }
-        );
-    });
-  });
-
-  function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName(
-      'homepage__services__tabcontent'
-    );
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = 'none';
-    }
-    tablinks = document.getElementsByClassName('tablinks');
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(' active', '');
-    }
-    document.getElementById(cityName).style.display = 'block';
-    evt.currentTarget.className += ' active';
-  }
-  document.getElementById('defaultOpen').click();
-
-}
 
 if (document.getElementById('menu__container')) {
 
@@ -139,4 +26,91 @@ if (document.getElementById('menu__container')) {
     document.getElementById('menu_contact').style.backgroundColor = "#f8d000";
   };
 
+}
+
+if (document.getElementById('homepage')) {
+
+  // carosel for logo in banner (nam)
+  let items = document.querySelectorAll(
+    ".hompage__banner__logo .carousel .carousel-item"
+  );
+
+  items.forEach((el) => {
+    const minPerSlide = 5;
+    let next = el.nextElementSibling;
+    for (var i = 1; i < minPerSlide; i++) {
+      if (!next) {
+        // wrap carousel by using first child
+        next = items[0];
+      }
+      let cloneChild = next.cloneNode(true);
+      el.appendChild(cloneChild.children[0]);
+      next = next.nextElementSibling;
+    }
+  });
+
+  //carosel for perfect service
+  // var slideIndex = 0;
+  // showSlides();
+
+  // function showSlides() {
+  //   var i;
+  //   var slides = document.getElementsByClassName("homepage__reviews__mySlides");
+  //   var dots = document.getElementsByClassName("homepage__reviews__dot");
+  //   for (i = 0; i < slides.length; i++) {
+  //     slides[i].style.display = "none";
+  //   }
+  //   slideIndex++;
+  //   if (slideIndex > slides.length) { slideIndex = 1 }
+  //   for (i = 0; i < dots.length; i++) {
+  //     dots[i].className = dots[i].className.replace("activeperfect", "");
+  //   }
+  //   slides[slideIndex - 1].style.display = "block";
+  //   dots[slideIndex - 1].className += " activeperfect";
+  //   setTimeout(showSlides, 2000);
+  // }
+
+  // services
+  $(document).ready(function () {
+    $('.counter').each(function () {
+      $(this)
+        .prop('Counter', 0)
+        .animate(
+          {
+            Counter: $(this).text(),
+          },
+          {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+              $(this).text(Math.ceil(now));
+            },
+          }
+        );
+    });
+  });
+
+  // tabpane in homepage service
+  $(window).resize(function () {
+    window.location.reload();
+  });
+
+  function openCity(evt, cityName) {
+    if (screen.width > 576) {
+      var i, tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName(
+        'homepage__services__tabcontent'
+      );
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = 'none';
+      }
+      tablinks = document.getElementsByClassName('tablinks');
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(' active', '');
+      }
+      document.getElementById(cityName).style.display = 'block';
+      evt.currentTarget.className += ' active';
+    }
+  }
+  document.getElementById('defaultOpen').click();
 }
